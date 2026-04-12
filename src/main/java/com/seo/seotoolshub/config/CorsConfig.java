a8ecad35a4df69805f0ffa7e22c7769b3152b45c
git remote add origin https://github.com/gudurusharathkumar/Rev_Training.git <gudurusharathkumar2002@gmail.com>
2026-04-12 11:18:56 +0530

    updated cors

--- src/main/java/com/seo/seotoolshub/config/CorsConfig.java
+++ src/main/java/com/seo/seotoolshub/config/CorsConfig.java
@@ -13,10 +13,15 @@ public class CorsConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
+
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE")
-                        .allowedHeaders("*");
+                        .allowedOrigins(
+                                "http://localhost:3000",
+                                "https://smart-seo-toolkit.vercel.app"
+                        )
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(false);
             }
         };
     }
